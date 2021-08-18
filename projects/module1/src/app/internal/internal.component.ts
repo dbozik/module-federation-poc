e3e105344e4633609d28064103609070ce95c1eb
Daniel Bozik <daniel.bozik@solargis.com>
2021-08-18 15:06:26 +0200

    internal component

---
+++ projects/module1/src/app/internal/internal.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-internal',
+  templateUrl: './internal.component.html',
+  styleUrls: ['./internal.component.scss']
+})
+export class InternalComponent {
+}
--- projects/shell/src/app/app-routing.module.ts
+++ projects/shell/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
     path: 'module1',
+    // loadChildren: () => loadRemoteModule({
+    //   remoteEntry: 'http://localhost:3000/remoteEntry.js',
+    //   remoteName: 'module1',
+    //   exposedModule: './module1'
+    // }),
     loadChildren: () => import('module1/Module').then(m => m.AppModule)
   },
 ];
